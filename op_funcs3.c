60c963c8a644ee453f3eeb3bc6455dd22ee4671e
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-14 19:48:24 +0100

    Update op_funcs3.c

--- op_funcs3.c
+++ op_funcs3.c
@@ -1,152 +1,79 @@
 #include "monty.h"
 
 /**
- * _mod - computes the rest of the division of the second top element of
- * the stack by the top element of the stack.
- * @stack: Pointer to head of stack (or queue)
- * @line_number: Current line number
- *
- * Return: No Return
- */
-void _mod(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp = *stack;
-	int count = 0, tmpn;
-
-	while (tmp)
-		tmp = tmp->next, count++;
-	if (count < 2)
-	{
-		dprintf(2, "L%d: can't mod, stack too short\n", line_number);
-		exit_op();
-		exit(EXIT_FAILURE);
-	}
-
-	tmpn = (*stack)->n;
-	delete_node_index(stack, 0);
-
-	if (tmpn == 0)
-	{
-		dprintf(2, "L%d: division by zero\n", line_number);
-		exit_op();
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->n %= tmpn;
-}
-/**
- * _pchar - prints the char at the top of the stack, followed by a new line
- * @stack: Pointer to head of stack (or queue)
- * @line_number: Current line number
- *
- * Return: No Return
- */
-void _pchar(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp = *stack;
-	int chr;
-
-	if (!stack || !(*stack))
-	{
-		dprintf(2, "L%d: can't pchar, stack empty\n", line_number);
-		exit_op();
-		exit(EXIT_FAILURE);
-	}
-
-	chr = tmp->n;
-	if (chr < 32 || chr > 126)
-	{
-		dprintf(2, "L%d: can't pchar, value out of range\n", line_number);
-		exit_op();
-		exit(EXIT_FAILURE);
-	}
-
-	printf("%c\n", tmp->n);
-
-}
-/**
- * _pstr - prints the string starting at the top of the stack, followed
- * by a new line
- * top element of the stack
- * @stack: Pointer to head of stack (or queue)
- * @line_number: Current line number
- *
- * Return: No Return
+ * _pstr - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
  */
 void _pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
-	int chr;
+	int c = 0;
 
-	(void) line_number;
-	while (stack && tmp)
+	(void)line_number;
+
+
+	while (tmp)
 	{
-		chr = tmp->n;
-		if (chr < 1 || chr > 127)
+		c = tmp->n;
+		if (c == 0 || _isalpha(c) == 0)
 			break;
-		printf("%c", chr);
+		putchar(c);
 		tmp = tmp->next;
 	}
-	printf("\n");
-
+	putchar('\n');
 }
+
 /**
- * _rotl - rotates the stack to the top
- * element of the stack
- * @stack: Pointer to head of stack (or queue)
- * @line_number: Current line number
- *
- * Return: No Return
+ * _rotl - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
  */
 void _rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack, *last = *stack, *lastcp;
+	stack_t *runner = *stack;
 
-	(void) line_number;
 
-	if (!stack || !(*stack) || !(*stack)->next)
+	int aux1 = 0;
+
+	if (!line_number || !stack || !*stack || !(*stack)->next)
 		return;
 
-	while (last->next)
-		last = last->next;
+	aux1 = runner->n;
 
-	*stack = tmp->next;
-	(*stack)->prev = tmp->prev;
+	while (runner->next)
+	{
+		runner = runner->next;
+		runner->prev->n = runner->n;
+	}
 
-	lastcp = last;
-
-	tmp->next = lastcp->next;
-	last->next = tmp;
-	tmp->prev = last;
-
-
+	runner->n = aux1;
 }
+
 /**
- * _rotr - rotates the stack to the bottom
- * of the stack.
- * @stack: Pointer to head of stack (or queue)
- * @line_number: Current line number
- *
- * Return: No Return
+ * _rotr - mod top of stack y second top stacks
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
  */
 void _rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack, *last = *stack, *prelast;
+	stack_t *runner = *stack;
 
-	(void) line_number;
+	int aux1 = 0;
 
-	if (!stack || !(*stack) || !(*stack)->next)
+	if (!line_number || !stack || !*stack || !(*stack)->next)
 		return;
 
-	while (last->next)
-		last = last->next;
+	while (runner->next)
+		runner = runner->next;
 
-	prelast = last->prev;
-	prelast->next = last->next;
+	aux1 = runner->n;
 
-	last->next = tmp;
-	last->prev = tmp->prev;
-	tmp->prev = last;
+	while (runner->prev)
+	{
+		runner = runner->prev;
+		runner->next->n = runner->n;
+	}
 
-	*stack = last;
+	runner->n = aux1;
 }
