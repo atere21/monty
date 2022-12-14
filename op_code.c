e6feb730aa20822b85d6dff8c5b67e7316f53f51
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-14 13:32:43 +0100

    Update op_code.c

--- op_code.c
+++ op_code.c
@@ -3,54 +3,54 @@
 #include "monty.h"
 
 /**
- *  * get_opcode - obtains the corresponding opcode and calls func
- *   * @opcode: string of opcode to be evaluated
- *    *
- *     * Return: O on success, or possible exit if
- *      * invalid instruction is given
- *       */
+ * get_opcode - obtains the corresponding opcode and calls func
+ * @opcode: string of opcode to be evaluated
+ *
+ * Return: O on success, or possible exit if
+ * invalid instruction is given
+ */
 int get_opcode(char *opcode)
 {
-		int i;
-			instruction_t ops[] = {
-						{"push", _push},
-								{"pall", _pall},
-										{"pint", _pint},
-												{"pop", _pop},
-														{"swap", _swap},
-																{"add", _add},
-																		{"nop", _nop},
-																				{"sub", _sub},
-																						{"div", _div},
-																								{"mul", _mul},
-																										{"mod", _mod},
-																												{"pchar", _pchar},
-																														{"pstr", _pstr},
-																																{"rotl", _rotl},
-																																		{"rotr", _rotr},
-																																				{"stack", _stack},
-																																						{"queue", _queue},
-																																								{NULL, NULL}
-							};
+	int i;
+	instruction_t ops[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
 
-				for (i = 0; ops[i].opcode && opcode; i++)
-							if (!strcmp(opcode, ops[i].opcode))
-										{
+	for (i = 0; ops[i].opcode && opcode; i++)
+		if (!strcmp(opcode, ops[i].opcode))
+		{
 
-														ops[i].f(&(globm.head), globm.line_number);
-																	return (0);
-																			}
+			ops[i].f(&(globm.head), globm.line_number);
+			return (0);
+		}
 
-					dprintf(2, "L%d: unknown instruction %s\n",
-									globm.line_number, opcode);
-						exit(EXIT_FAILURE);
+	dprintf(2, "L%d: unknown instruction %s\n",
+		globm.line_number, opcode);
+	exit(EXIT_FAILURE);
 }
 /**
- *  * exit_op - performs exit operations (free/close)
- *   * Return: No Return
- *    */
+ * exit_op - performs exit operations (free/close)
+ * Return: No Return
+ */
 void exit_op(void)
 {
-		free(globm.gbuff), fclose(globm.fp);
-			free_stack_t(globm.head);
+	free(globm.gbuff), fclose(globm.fp);
+	free_stack_t(globm.head);
 }
