1a6c47ff4ca190d6bb7eea47b4b009f170e22e2d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-14 13:27:11 +0100

    Update doubly_ll.c

--- doubly_ll.c
+++ doubly_ll.c
@@ -1,4 +1,4 @@
-#include monty.h
+#include "monty.h"
 
 /**
  * add_dnodeint - adds a new node at the beginning of a stack_t list.
@@ -9,24 +9,26 @@
  */
 stack_t *add_dnodeint(stack_t **head, const int n)
 {
-stack_t *new_node;
+	stack_t *new_node;
 
-return (NULL);
+	if (!head)
+		return (NULL);
 
-new_node = malloc(sizeof(stack_t));
-{
-dprintf(2, Error: malloc failedn);
-exit_op();
-exit(EXIT_FAILURE);
-}
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = *head;
-if (*head)
-(*head)->prev = new_node;
-*head = new_node;
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		exit_op();
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
 
-return (new_node);
+	return (new_node);
 }
 /**
  * add_dnodeint_end - adds a new node at the end of a stack_t list.
@@ -37,28 +39,31 @@ return (new_node);
  */
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-stack_t *new_node, *tmp = *head;
+	stack_t *new_node, *tmp = *head;
 
-return (NULL);
+	if (!head)
+		return (NULL);
 
-new_node = malloc(sizeof(stack_t));
-{
-dprintf(2, Error: malloc failedn);
-exit_op();
-exit(EXIT_FAILURE);
-}
-new_node->n = n;
-{
-new_node->prev = NULL, new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
-while (tmp->next)
-tmp = tmp->next;
-new_node->prev = tmp;
-new_node->next = NULL;
-tmp->next = new_node;
-return (new_node);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		exit_op();
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	if (!(*head))
+	{
+		new_node->prev = NULL, new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	while (tmp->next)
+		tmp = tmp->next;
+	new_node->prev = tmp;
+	new_node->next = NULL;
+	tmp->next = new_node;
+	return (new_node);
 }
 /**
  * delete_node_index - deletes the node at index
@@ -69,32 +74,33 @@ return (new_node);
  */
 int delete_node_index(stack_t **head, unsigned int index)
 {
-stack_t *tmpcount = *head, *tmp1 = *head, *tmp2, *tmp3;
-unsigned int count = 0;
+	stack_t *tmpcount = *head, *tmp1 = *head, *tmp2, *tmp3;
+	unsigned int count = 0;
 
-return (-1);
-while (tmpcount)
-tmpcount = tmpcount->next, count++;
-if (index > (count - 1))
-return (-1);
-if (index == 0)
-{
-tmp1 = tmp1->next;
-free(*head);
-*head = tmp1;
-if (*head)
-(*head)->prev = NULL;
-return (1);
-}
-for (count = 0; count < index; count++)
-tmp2 = tmp1, tmp1 = tmp1->next;
+	if (!head || !(*head))
+		return (-1);
+	while (tmpcount)
+		tmpcount = tmpcount->next, count++;
+	if (index > (count - 1))
+		return (-1);
+	if (index == 0)
+	{
+		tmp1 = tmp1->next;
+		free(*head);
+		*head = tmp1;
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
+	}
+	for (count = 0; count < index; count++)
+		tmp2 = tmp1, tmp1 = tmp1->next;
 
-tmp2->next = tmp1->next;
-tmp3 = tmp1->next;
-if (tmp3)
-tmp3->prev = tmp2;
-free(tmp1);
-return (1);
+	tmp2->next = tmp1->next;
+	tmp3 = tmp1->next;
+	if (tmp3)
+		tmp3->prev = tmp2;
+	free(tmp1);
+	return (1);
 }
 /**
  * free_stack_t - function that frees a stack_t list.
@@ -104,12 +110,12 @@ return (1);
  */
 void free_stack_t(stack_t *head)
 {
-stack_t *tmp;
+	stack_t *tmp;
 
-while (head)
-{
-tmp = head;
-head = head->next;
-free(tmp);
-}
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
