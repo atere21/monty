77bc2c38d2e1fe4ef0a2bb07d8d55ed6f39d0db6
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-15 18:26:38 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,50 +1,48 @@
 #include "monty.h"
-
 /**
-* f_queue - function that prints the top of the queue
-* @head: head of queue
-* @counter: line count
-*
-* Return: nothing
+ * f_queue - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
 */
 void f_queue(stack_t **head, unsigned int counter)
 {
-(void)head;
-(void)counter;
-bus.lifi = 1;
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
 }
 
 /**
-* addqueue - function that add node to the tail the queue
-* @n: new value
-* @head: head of the queue
-*
-* Return: nothing
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
 */
 void addqueue(stack_t **head, int n)
 {
-stack_t *new_node, *temp;
+	stack_t *new_node, *aux;
 
-temp = *head;
-new_node = malloc(sizeof(stack_t));
-if (new_node == NULL)
-{
-printf(Errorn);
-}
-new_node->n = n;
-new_node->next = NULL;
-if (temp)
-{
-while (temp->next)
-temp = temp->next;
-}
-{
-*head = new_node;
-new_node->prev = NULL;
-}
-else
-{
-temp->next = new_node;
-new_node->prev = temp;
-}
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
 }
