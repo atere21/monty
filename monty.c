a808427d5c5f6bfcc4f7eb2a293741621f61516d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-15 16:48:26 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,63 +1,24 @@
 #include "monty.h"
-
-global_m globm;
-void set_global(void);
-
+global_var var_global;
 /**
- * main -  monty interpreter
- * @ac: command line arguments count
- * @av: command line arguments array of strings
- *
- * Return: 0 if success
+ * main - driver function for monty program
+ * @ac: int num of arguments
+ * @av: opcode file
+ * Return: 0
  */
 int main(int ac, char **av)
 {
-	char *buff = NULL, *dlim = " \n\t", *optok = NULL;
-	size_t buff_size = 0;
-	ssize_t line_size;
-	FILE *fp;
+	stack_t *stack;
 
+	stack = NULL;
 	if (ac != 2)
-		dprintf(2, "USAGE: monty file\n"), exit(EXIT_FAILURE);
-
-	fp  = fopen(av[1], "r");
-	if (!fp)
 	{
-		dprintf(2, "Error: Can't open file %s\n", av[1]);
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	globm.fp = fp;
-	set_global();
-	line_size = getline(&buff, &buff_size, globm.fp);
-	globm.gbuff = buff;
-	while (line_size >= 0)
-	{
-		globm.line_number += 1;
-		optok = strtok(buff, dlim);
-
-		if (optok && optok[0] != '#')
-		{
-			globm.n = strtok(NULL, dlim);
-			get_opcode(optok);
-		}
-		line_size = getline(&buff, &buff_size, globm.fp);
-		optok = NULL, globm.n = NULL;
-	}
-	exit_op();
+	read_file(av[1], &stack);
+    /* recordar liberar memorias */
+	free_dlistint(stack);
 	return (0);
-}
-
-/**
- * set_global - defines global variables
- *
- * Return: No return
- */
-void set_global(void)
-{
-	globm.mode = 0;
-	globm.gbuff = NULL;
-	globm.n = NULL;
-	globm.head = NULL;
-	globm.line_number = 0;
 }
